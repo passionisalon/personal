b35bc9e34c1c745ef1398253102b84c89fcefc03
choikyuho <intelinger@gmail.com>
2023-07-15 15:24:54 +0900

    230715_2

--- vuejs3Windows/sample2/src/App.vue
+++ vuejs3Windows/sample2/src/App.vue
@@ -1,14 +1,49 @@
+<!-- 
+  글 목록페이지,
+  상세페이지 만들 예정 페이지 2~3개정도 만들어오기
+
+ -->
+
 <template>
-  
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link disabled">Disabled</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div>
+      <h4>개발자의 블로그입니다.</h4>
+      <p>Vue로 만들었음</p>
+    </div>
+
+    <ListComponent v-bind:blogData="blogData"/>
+
+  </div>
 </template>
 
 <script>
-
-
+import ListComponent from './components/ListComponent.vue'
+import blog from './assets/blog.js'
 export default {
   name: 'App',
+  data(){
+    return{
+      blogData : blog,
+    }
+  },
   components: {
-    
+    ListComponent:ListComponent,
   }
 }
 </script>
@@ -20,6 +55,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ vuejs3Windows/sample2/src/components/ListComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div v-for='(pros,index) in blogData' :key="index">
+            <h4>{{pros.title}}</h4>
+            <p>{{ pros.content }}</p>
+            <p>{{pros.date}}</p>
+        </div>
+        <hr>
+        <div>
+            <h4>{{blogData[0].title}}</h4>
+            <p>{{ blogData[0].content }}</p>
+            <p>{{blogData[0].date}}</p>
+        </div>
+        <div>
+            <h4>{{blogData[1].title}}</h4>
+            <p>{{ blogData[1].content }}</p>
+            <p>{{blogData[1].date}}</p>
+        </div>
+        <div>
+            <h4>{{blogData[2].title}}</h4>
+            <p>{{ blogData[2].content }}</p>
+            <p>{{blogData[2].date}}</p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default{
+    props:{
+        blogData : Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
