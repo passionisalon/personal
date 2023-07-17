ca255d27461bfb18df7698197d708d28b35fdfa4
choikyuho <intelinger@gmail.com>
2023-07-17 14:09:19 +0900

    230717_1

---
+++ vuejs3Windows/sample3/src/App.vue
@@ -0,0 +1,144 @@
+<!-- 
+ - 다른 곳에 서 쓸 것 같으면 컴포넌트
+ - 라워로 나눌 페이지도 컴포넌트
+ - 혹은 HTML이 너무 길어서 복잡하면 컴포넌트
+ -->
+ <!-- 숙제
+1. App.vue에 인스타 데이터 저장
+2. Post.Vue에 {{데이터바인딩}}
+-->
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <ContainerComponent v-bind:infodata="infodata"/>
+
+  <!--  -->
+
+  <!-- <div class="sample-box">임시 박스</div> -->
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+</template>
+
+<script>
+import datas from './assets/Data.js';
+
+import ContainerComponent from './components/ContainerComponent';
+
+
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      infodata : datas,
+    }
+  },
+  components: {
+    ContainerComponent : ContainerComponent,
+
+  }
+}
+</script>
+
+<style>
+@import './style.css';
+body {
+  margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+
+.inputfile {
+  display: none;
+}
+
+.input-plus {
+  cursor: pointer;
+}
+
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}</style>
---
+++ vuejs3Windows/sample3/src/components/ContainerComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-for="(pros,index) in infodata" :key="index">
+        <PostComponent v-bind:pros="pros"/>
+    </div>
+    <hr>
+    <!-- <div>
+        <PostComponent v-bind:pros="infodata[0]"/>
+        <PostComponent v-bind:pros="infodata[1]"/>
+        <PostComponent v-bind:pros="infodata[2]"/>
+    </div> -->
+</template>
+
+<script>
+import PostComponent from './PostComponent.vue';
+export default {
+    props:{
+        infodata:Object,
+    },
+    components:{
+        PostComponent : PostComponent,
+    }
+}
+</script>
+
+<style>
+
+</style>
