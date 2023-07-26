90e2b2fd7c7e1d285160a5719bd006422384863a
choikyuho <intelinger@gmail.com>
2023-07-26 15:23:32 +0900

    230726_2

---
+++ ex2/src/main/java/org/zerock/ex2/entity/Memo.java
@@ -0,0 +1,14 @@
+package org.zerock.ex2.entity;
+
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="tbl_memo")
+@ToString
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+}
