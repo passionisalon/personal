1b951fc01eae937c0a8be04670cd59f46af13d1a
choikyuho <intelinger@gmail.com>
2023-09-30 19:07:57 +0900

    230930_1

--- study/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ study/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -29,4 +29,14 @@ public class HelloController {
         model.addAttribute("data","spring!!!");
         return "hello";
     }   // end hello
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+        this.thisClassInfo();
+        logger.info("helloMvc(name : {}, model : {}) invoked.",name,model);
+        model.addAttribute("name",name);
+
+        return "hello-template";
+    }   // end helloMvc
+
 }   // end class
