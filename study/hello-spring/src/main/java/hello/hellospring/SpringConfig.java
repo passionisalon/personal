00dbec10561ad71fc4e2c660558a7ee40a774c7a
choikyuho <intelinger@gmail.com>
2023-10-06 16:37:45 +0900

    231006_1

--- study/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ study/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -19,27 +19,36 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
+    // SpringDataJpaMemberRepository를 만들면 아래의 @Bean public MemberReepository memberRepositoryBean()을 없애도 구동이 된다.
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberServiceBean memberService(){
-        return new MemberServiceBean(this.memberRepositoryBean());
+
+//        return new MemberServiceBean(this.memberRepositoryBean());
+        return new MemberServiceBean(this.memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepositoryBean(){
+//    @Bean
+//    public MemberRepository memberRepositoryBean(){
 //        return new MemoryMemberRepositoryBean();
 //        return new JdbcMemberRepository(this.dataSource);
 //        return new JdbcTemplateMemberRepository(this.dataSource);
-        return new JpaMemberRepository(this.em);
-    }
+//        return new JpaMemberRepository(this.em);
+//    }
 
 
 }   // end class
